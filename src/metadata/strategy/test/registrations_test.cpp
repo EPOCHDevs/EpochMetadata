b1bc601e31805c45693b7b86a6e0ae071007332e
adesola <ava6969@rit.edu>
2025-02-04 17:14:53 -0800

    updated test

--- src/metadata/strategy/test/registrations_test.cpp
+++ src/metadata/strategy/test/registrations_test.cpp
@@ -10,9 +10,9 @@ TEST_CASE("Strategy Component MetaData Total Count is Correct")
     using namespace metadata::strategy;
     REQUIRE(slippage::Registry::GetInstance().GetMetaData().size() == 3);
     REQUIRE(commission::Registry::GetInstance().GetMetaData().size() == 2);
-    REQUIRE(futures_continuation::Registry::GetInstance().GetMetaData().size() == 1);
-    REQUIRE(take_profit::Registry::GetInstance().GetMetaData().size() == 5);
-    REQUIRE(stop_loss::Registry::GetInstance().GetMetaData().size() == 5);
-    REQUIRE(position_sizer::Registry::GetInstance().GetMetaData().size() == 10);
+    REQUIRE(futures_continuation::Registry::GetInstance().GetMetaData().size() == 3);
+    REQUIRE(take_profit::Registry::GetInstance().GetMetaData().size() == 4);
+    REQUIRE(stop_loss::Registry::GetInstance().GetMetaData().size() == 4);
+    REQUIRE(position_sizer::Registry::GetInstance().GetMetaData().size() == 9);
     REQUIRE(trade_signal::Registry::GetInstance().GetMetaData().size() == 8);
 }
