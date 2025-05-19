9ec3e80c97ef40befe3f6c000da5490386942592
adesola <ava6969@rit.edu>
2025-05-19 11:10:46 -0700

    created csv

--- test/transforms/registrations_test.cpp
+++ test/transforms/registrations_test.cpp
@@ -5,10 +5,58 @@
 #include "epoch_metadata/transforms/registration.h"
 #include "epoch_metadata/transforms/registry.h"
 #include <catch.hpp>
+#include <fstream>
+#include <glaze/glaze.hpp>
+
+#ifdef GLZ_ENABLE_JSON_PRETTY_PRINT
+constexpr bool kWriteToCsv = true;
+#else
+constexpr bool kWriteToCsv = false;
+#endif
 
 TEST_CASE("Transform MetaData Total Count is Correct") {
   using namespace epoch_metadata::transforms;
   RegisterTransformMetadata(epoch_metadata::DEFAULT_YAML_LOADER);
 
-  REQUIRE(ITransformRegistry::GetInstance().GetMetaData().size() == 226);
+  auto metadata = ITransformRegistry::GetInstance().GetMetaData();
+  REQUIRE(metadata.size() == 226);
+
+  if constexpr (kWriteToCsv) {
+
+    std::ofstream csvStream("transforms.csv");
+    csvStream
+        << "ID;Category;RenderKind;PlotKind;Name;Description;Options;Inputs;"
+           "Outputs;Tags;AtLeastOneInputRequired;RequiresTimeFrame;"
+           "IsCrossSectional\n";
+
+    auto pretty_tags = [](auto const &obj) {
+      std::stringstream ss;
+      for (auto const &tag : obj) {
+        ss << tag << ", ";
+      }
+      return ss.str();
+    };
+
+    auto pretty_ids = [](auto const &objs) {
+      std::stringstream ss;
+      for (auto const &obj : objs) {
+        ss << obj.id << ", ";
+      }
+      return ss.str();
+    };
+
+    for (auto const &transform : metadata | std::views::values) {
+      csvStream << transform.id << ";" << transform.category << ";"
+                << transform.renderKind << ";" << transform.plotKind << ";"
+                << transform.name << ";" << transform.desc << ";"
+                << pretty_ids(transform.options) << ";"
+                << pretty_ids(transform.inputs) << ";"
+                << pretty_ids(transform.outputs) << ";"
+                << pretty_tags(transform.tags) << ";" << std::boolalpha
+                << transform.atLeastOneInputRequired << ";"
+                << transform.requiresTimeFrame << ";"
+                << transform.isCrossSectional << std::noboolalpha << "\n";
+    }
+    csvStream.close();
+  }
 }
