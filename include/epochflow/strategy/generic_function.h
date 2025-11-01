c04c5f5ed2695808accd8c9dc05920877eb7da06
adesola <ava6969@rit.edu>
2025-11-01 11:49:21 -0700

    added include epochflow

---
+++ include/epochflow/strategy/generic_function.h
@@ -0,0 +1,39 @@
+//
+// Created by adesola on 1/27/25.
+//
+
+#pragma once
+#include <epochflow/core/metadata_options.h>
+#include <epochflow/strategy/metadata.h>
+#include <glaze/glaze.hpp>
+
+CREATE_ENUM(GenericFunctionAuthor, User, Epoch);
+
+namespace epochflow::strategy
+{
+  struct GenericFunction
+  {
+    std::optional<std::string> type{};
+    std::optional<epochflow::MetaDataArgDefinitionMapping> args{};
+    std::optional<epochflow::TimeFrame> timeframe{};
+    std::optional<PythonSource> source{};
+    glz::generic kwarg{};
+
+    bool operator==(const GenericFunction &other) const
+    {
+      return (type == other.type) && (args == other.args) &&
+             (timeframe == other.timeframe) &&
+             (source == other.source) &&
+             (glz::write_json(kwarg) == glz::write_json(other.kwarg));
+    }
+  };
+
+  template <typename T>
+  struct TemplatedGenericFunction
+  {
+    T type;
+    MetaDataArgDefinitionMapping args;
+  };
+  bool EqualsOptionalGenericFunction(std::optional<GenericFunction> const &lhs,
+                                     std::optional<GenericFunction> const &rhs);
+} // namespace epochflow::strategy
